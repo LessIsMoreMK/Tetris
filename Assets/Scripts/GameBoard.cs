196682f3c91f35b84921018dc37521ff16d9c339
LessIsMoreMK <maciej8kz@gmail.com>
2020-03-04 22:50:38 +0100

    Stacking shapes

---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameBoard : MonoBehaviour
+{
+    public static Transform[,] gameBoard = new Transform[10, 20];
+
+}
--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -2,85 +2,150 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shape : MonoBehaviour {
-
+public class Shape : MonoBehaviour
+{
     public float speed = 1.0f;
 
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown("a"))
+    float lastMoveDown = 0;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown("a"))
         {
             transform.position += new Vector3(-1, 0, 0);
 
-            Debug.Log(transform.position);
-
-            if(!IsInGrid())
+            if (!IsInGrid())
             {
                 transform.position += new Vector3(1, 0, 0);
             }
+            else
+            {
+                UpdateGameBoard();
+            }
         }
+
         if (Input.GetKeyDown("d"))
         {
             transform.position += new Vector3(1, 0, 0);
-
-            Debug.Log(transform.position);
 
             if (!IsInGrid())
             {
                 transform.position += new Vector3(-1, 0, 0);
             }
+            else
+            {
+                UpdateGameBoard();
+            }
         }
-        if (Input.GetKeyDown("s"))
+        if (Input.GetKeyDown("s") || Time.time - lastMoveDown >= 1)
         {
             transform.position += new Vector3(0, -1, 0);
-
-            Debug.Log(transform.position);
 
             if (!IsInGrid())
             {
                 transform.position += new Vector3(0, 1, 0);
+
+                enabled = false;
+
+                FindObjectOfType<ShapeSpawner>().SpawnShape();
             }
+            else
+            {
+                UpdateGameBoard();
+            }
+
+            lastMoveDown = Time.time;
         }
+
         if (Input.GetKeyDown("w"))
         {
             transform.Rotate(0, 0, 90);
-
-            Debug.Log(transform.position);
 
             if (!IsInGrid())
             {
                 transform.Rotate(0, 0, -90);
             }
-        }
-    }
-    
-    public bool IsInGrid()
-    {
-        int childCount = 0;
-
-        foreach(Transform childBlock in transform)
-        {
-            Vector2 vect = childBlock.position;
-            childCount++;
-
-            Debug.Log(childCount + " " + childBlock.position);
-
-            if(!IsInBorder(vect))
+            else
             {
-                return false;
+                UpdateGameBoard();
             }
         }
 
+        if (Input.GetKeyDown("e"))
+        {
+            transform.Rotate(0, 0, -90);
+
+            if (!IsInGrid())
+            {
+                transform.Rotate(0, 0, 90);
+            }
+            else
+            {
+                UpdateGameBoard();
+            }
+        }
+
+    }
+
+    public bool IsInGrid()
+    {
+        foreach (Transform childBlock in transform)
+        {
+            Vector2 vect = RoundVector(childBlock.position);
+
+            if (!IsInBorder(vect))
+            {
+                return false;
+            }
+
+
+            if (GameBoard.gameBoard[(int)vect.x, (int)vect.y] != null &&
+                GameBoard.gameBoard[(int)vect.x, (int)vect.y].parent != transform)
+            {
+                return false;
+            }
+
+        }
         return true;
+    }
+
+    public Vector2 RoundVector(Vector2 vect)
+    {
+        return new Vector2(Mathf.Round(vect.x), Mathf.Round(vect.y));
     }
 
     public static bool IsInBorder(Vector2 pos)
     {
-        return ((int)pos.x >= -4.2620001 && (int)pos.x <= 4.741 &&
-            (int)pos.y >= -9.9427488);
+        return ((int)pos.x >= 0 &&
+            (int)pos.x <= 9 &&
+            (int)pos.y >= 2);
     }
+
+    public void UpdateGameBoard()
+    {
+        for (int y = 0; y < 20; ++y)
+        {
+            for (int x = 0; x < 10; ++x)
+            {
+                if (GameBoard.gameBoard[x, y] != null && GameBoard.gameBoard[x, y].parent == transform)
+                {
+                    GameBoard.gameBoard[x, y] = null;
+                }
+
+            }
+        }
+
+        foreach (Transform childBlock in transform)
+        {
+            Vector2 vect = RoundVector(childBlock.position);
+
+            GameBoard.gameBoard[(int)vect.x, (int)vect.y] = childBlock;
+        }
+    }
+
 }
