8cc37f4797be524e610df5e1dec5c92dee3ae50a
LessIsMoreMK <maciej8kz@gmail.com>
2020-03-04 13:57:16 +0100

    Initial commit

---
+++ Assets/Scripts/ShapeSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeSpawner : MonoBehaviour {
+
+    public GameObject[] shapes;
+
+    public void SpawnShape()
+    {
+        int ShapeIndex = Random.Range(0, 6);
+        Instantiate(shapes[ShapeIndex], transform.position, Quaternion.identity);
+    }
+
+    private void Start()
+    {
+        SpawnShape();
+    }
+}
